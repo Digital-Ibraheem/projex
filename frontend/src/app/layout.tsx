f36f30096c50200a25db0ac3b2c8716e0a280b5f
digital-ibraheem <ibraheemd101@gmail.com>
2025-02-22 15:58:06 -0500

    New branch for create page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,9 +3,11 @@ import { Inter, Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/root/Navbar";
 import Footer from "@/components/root/Footer";
+import FooterVisibilityWrapper from "@/components/root/FooterVisibilityWrapper"; // New Wrapper
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const roboto = Roboto({ weight: "400", subsets: ["latin"], variable: "--font-roboto" });
+
 
 export const metadata: Metadata = {
   title: "Projex",
@@ -16,11 +18,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={`${inter.variable} ${roboto.variable}`}>
       <body className="antialiased">
-      <Navbar />
+        <Navbar />
         <main tabIndex={0} className="pt-[90px]"> {/* Adjust this value based on your navbar height */}
           {children}
         </main>
-        <Footer />
+        <FooterVisibilityWrapper />
       </body>
     </html>
   );
